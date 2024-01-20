359d0a6b0e6bd5ae9368097eef1d04290d8456c6
Nahiyan Ishtiaque <nahiyan.ishtiaque@gmail.com>
2024-01-19 23:44:04 -0500

    completed frontend of website

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=b7d21bb44af1e27256b893737fb76d68&page=1';
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=b7d21bb44af1e27256b893737fb76d68&query=";
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
-const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&dapi_key=b7d21bb44af1e27256b893737fb76d68&query=";
 
 const main = document.getElementById("section");
 const form = document.getElementById("form");
@@ -15,15 +15,32 @@ function returnMovies(url) {
             console.log(data.results);
             data.results.forEach(element => {
                 const div_card = document.createElement('div');
+                div_card.setAttribute('class', 'card');
+                
                 const div_row = document.createElement('div');
+                div_row.setAttribute('class', 'row');
+                
                 const div_column = document.createElement('div');
+                div_column.setAttribute('class', 'column');
+                
                 const image = document.createElement('img');
-                const title = document.createElement('h2');
+                image.setAttribute('class', 'thumbnail');
+                image.setAttribute('id', 'image');
 
-                title.innerHTML = '${element.title}';
+                const title = document.createElement('h2');
+                title.setAttribute('id', 'title');
+
+                const center = document.createElement('center');
+
+                title.innerHTML = `${element.title}`;
                 image.src = IMG_PATH + element.poster_path;
+                
+                center.appendChild(image);
+                div_card.appendChild(center);
+                div_card.appendChild(title);
                 div_column.appendChild(div_card);
                 div_row.appendChild(div_column);
+                main.appendChild(div_row);
                 
             });
         })
@@ -31,3 +48,15 @@ function returnMovies(url) {
             console.error('Error fetching data:', error);
         });
 }
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    main.innerHTML = '';
+
+    const searchItem = search.value;
+
+    if (searchItem) {
+        returnMovies(SEARCHAPI + searchItem);
+        search.value = "";
+    }
+});
