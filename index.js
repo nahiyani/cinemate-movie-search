6e41500330c1246140d1a508f1f7e35496cee2c8
Nahiyan Ishtiaque <nahiyan.ishtiaque@gmail.com>
2024-01-20 19:05:59 -0500

    backend push

---
+++ index.js
@@ -0,0 +1,7 @@
+import app from "./server.js"
+import mongodb from "mongodb"
+import ReviewsDAO from "./dao/reviewsDAO.js"
+
+const MongoClient = mongodb.MongoClient;
+const mongo_username = process.env.MONGO_USERNAME;
+const mongo_password = process.env.MONGO_PASSWORD;
